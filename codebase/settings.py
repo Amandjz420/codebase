7d21425cd9ab4cd742a3fdcff25212f29a6fbdaf
Aman Agarwal <aman@Amans-MacBook-Pro.local>
2025-01-17 21:26:23 +0530

    changes

--- codebase/settings.py
+++ codebase/settings.py
@@ -41,9 +41,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://dev.codeknot.ai',
-    'http://localhost:5173/',
-    'http://127.0.0.1:3000/',
-    'http://127.0.0.1:5173/',
+    'http://localhost:5173',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:5173',
     'http://0.0.0.0:3000',
 ]
 
